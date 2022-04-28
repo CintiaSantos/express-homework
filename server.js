11a8db2c79403702a36b439a6c7caf7a150618fb
Cintia Santos <37563871+CintiaSantos@users.noreply.github.com>
2022-04-28 13:28:20 -0400

    created greetings route

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const PORT = 3000
+// respond with "hello world" when a GET request is made to the homepage
+app.get('/greetings/:name', (req, res) => {
+  res.send('hello there ' + req.params.name)
+})
+
+
+
+app.listen(PORT, () => {
+    console.log(`Example app listening on port ${PORT}`)
+  })
